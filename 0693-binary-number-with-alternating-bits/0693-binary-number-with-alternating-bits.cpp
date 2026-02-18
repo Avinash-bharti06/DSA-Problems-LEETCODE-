3ee95180ff4429d16443344815988258259972f9
Avinash Bharti <avinashbharti3007@gmail.com>
2026-02-18 23:09:30 +0530

    Time: 2 ms (2.89%), Space: 7.9 MB (10.25%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string bins(int n) {
-        if (n == 0) return "0";
-        string s = "";
-        while (n > 0) {
-            s.push_back('0' + (n % 2));
-            n /= 2;
-        }
-        reverse(s.begin(), s.end());
-        return s;
+    if (n == 0) return "0";
+    
+    string s = "";
+    while (n > 0) {
+        s.push_back('0' + (n % 2));  // remainder ko char me convert
+        n /= 2;
     }
-
+    reverse(s.begin(), s.end());
+    return s;
+}
     bool hasAlternatingBits(int n) {
         string t = bins(n);
-
-        for (int i = 1; i < (int)t.size(); i++) {
-            if (t[i] == t[i - 1]) return false; // adjacent same => not alternating
+        for(int i=1;i<t.size();i++){
+            if(t[i]==t[i-1]) return false;
         }
         return true;
     }
