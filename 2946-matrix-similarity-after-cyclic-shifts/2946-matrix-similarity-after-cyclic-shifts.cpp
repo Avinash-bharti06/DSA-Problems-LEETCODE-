242643b9314ff9074b7d01ff3ab18ae099b368bc
Avinash Bharti <avinashbharti3007@gmail.com>
2026-03-27 21:32:22 +0530

    Time: 1 ms (39.75%), Space: 32.7 MB (18.63%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -3,20 +3,37 @@ public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int m = mat.size();
         int n = mat[0].size();
-        
-        k = k % n;
-        vector<vector<int>> ans(m, vector<int>(n));
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+
+        if (k == 0) return true;
+
+        k = k % n;   // extra rotations hata do
+        vector<vector<int>> ans = mat;
+
+        int cnt = 1;
+        while (cnt <= k) {
+            for (int i = 0; i < m; i++) {
+
                 if (i % 2 == 0) {
-                    ans[i][j] = mat[i][(j + k) % n];      // left shift
-                } else {
-                    ans[i][j] = mat[i][(j - k + n) % n];  // right shift
+                    // even row -> left shift by 1
+                    int first = ans[i][0];
+                    for (int j = 0; j < n - 1; j++) {
+                        ans[i][j] = ans[i][j + 1];
+                    }
+                    ans[i][n - 1] = first;
                 }
+                else {
+                    // odd row -> right shift by 1
+                    int last = ans[i][n - 1];
+                    for (int j = n - 1; j > 0; j--) {
+                        ans[i][j] = ans[i][j - 1];
+                    }
+                    ans[i][0] = last;
+                }
+
             }
+            cnt++;
         }
-        
+
         return ans == mat;
     }
 };
