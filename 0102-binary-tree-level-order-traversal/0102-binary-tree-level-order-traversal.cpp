81a0df576c1c5e95bfa2d6eb3b7535edabcd8144
Avinash Bharti <avinashbharti3007@gmail.com>
2026-04-13 23:58:05 +0530

    Time: 0 ms (100%), Space: 16.6 MB (98.95%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,41 +1,30 @@
-    /**
-    * Definition for a binary tree node.
-    * struct TreeNode {
-    *     int val;
-    *     TreeNode *left;
-    *     TreeNode *right;
-    *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    * };
-    */
-    class Solution {
-    public:
+class Solution {
+public:
     int levels(TreeNode* root){
-        if(root==NULL) return 0;
-        return 1 + max(levels(root->left),levels(root->right));
+        if(root == NULL) return 0;
+        return 1 + max(levels(root->left), levels(root->right));
     }
-    void nthlevel(TreeNode* root,int curr,int level,vector<int>& v){
-        if(root==nullptr)return;
-        if(curr==level){
-            v.push_back(root->val);
-            return;
-        }
-        nthlevel(root->left,curr+1,level,v);
-        nthlevel(root->right,curr+1,level,v);
-    }
-    void lorder(TreeNode* root,vector<vector<int>>& ans){
-        int n= levels(root);
-        for(int i=1;i<=n;i++){
-            vector<int>v;
-            nthlevel(root,1,i,v);
-            ans.push_back(v);
 
-        }
+    void lOrder(TreeNode* root, vector<vector<int>>& ans, int level){
+        if(root == NULL) return;
+
+        ans[level].push_back(root->val);
+
+        lOrder(root->left, ans, level + 1);
+        lOrder(root->right, ans, level + 1);
     }
-        vector<vector<int>> levelOrder(TreeNode* root) {
-            vector<vector<int>>ans;
-            lorder(root,ans);
-            return ans;
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        int n = levels(root);
+        vector<vector<int>> ans;
+
+        for(int i = 1; i <= n; i++){
+            vector<int> v;
+            ans.push_back(v);
         }
-    };
+
+        lOrder(root, ans, 0);
+
+        return ans;
+    }
+};
