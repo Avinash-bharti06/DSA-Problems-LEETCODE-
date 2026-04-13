e62f61b166b1fa922f3a2beb16143451a74e3ff1
Avinash Bharti <avinashbharti3007@gmail.com>
2026-04-13 23:31:28 +0530

    Time: 10 ms (5.81%), Space: 17.1 MB (71.96%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,37 +1,41 @@
-class Solution {
-public:
+    /**
+    * Definition for a binary tree node.
+    * struct TreeNode {
+    *     int val;
+    *     TreeNode *left;
+    *     TreeNode *right;
+    *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    * };
+    */
+    class Solution {
+    public:
     int levels(TreeNode* root){
-        if(root == NULL) return 0;
-        return 1 + max(levels(root->left), levels(root->right));
+        if(root==NULL) return 0;
+        return 1 + max(levels(root->left),levels(root->right));
     }
+    void nthlevel(TreeNode* root,int curr,int level,vector<int>& v){
+        if(root==nullptr)return;
+        if(curr==level){
+            v.push_back(root->val);
+            return;
+        }
+        nthlevel(root->left,curr+1,level,v);
+        nthlevel(root->right,curr+1,level,v);
+    }
+    void lorder(TreeNode* root,vector<vector<int>>& ans){
+        int n= levels(root);
+        for(int i=1;i<=n;i++){
+            vector<int>v;
+            nthlevel(root,1,i,v);
+            ans.push_back(v);
 
-    vector<int> nlevel(TreeNode* root, int curr, int level){
-        vector<int> ans;
-        if(root == NULL) return ans;
-
-        if(curr == level){
-            ans.push_back(root->val);
+        }
+    }
+        vector<vector<int>> levelOrder(TreeNode* root) {
+            vector<vector<int>>ans;
+            lorder(root,ans);
             return ans;
         }
-
-        vector<int> left = nlevel(root->left, curr+1, level);
-        vector<int> right = nlevel(root->right, curr+1, level);
-
-        // merge left and right
-        ans.insert(ans.end(), left.begin(), left.end());
-        ans.insert(ans.end(), right.begin(), right.end());
-
-        return ans;
-    }
-
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        int level = levels(root);
-        vector<vector<int>> req;
-
-        for(int i = 1; i <= level; i++){
-            req.push_back(nlevel(root, 1, i));
-        }
-
-        return req;
-    }
-};
+    };
