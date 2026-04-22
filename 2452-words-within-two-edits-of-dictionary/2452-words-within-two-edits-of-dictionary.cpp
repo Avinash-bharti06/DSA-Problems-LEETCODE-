00462b35efcdbb545bfbe8a2d3b86e3424c9c78c
Avinash Bharti <avinashbharti3007@gmail.com>
2026-04-22 18:00:53 +0530

    Time: 19 ms (29.41%), Space: 25.1 MB (21.39%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    bool eqal(string s, string t) {
+        if (s.size() != t.size()) return false; 
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != t[i]) count++;
+            if (count >= 3) return false;  
+        }
+        return count < 3;
+    }
+
     vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-        
+        vector<string> ans;
+    
+        for (int i = 0; i < queries.size(); i++) {
+            for (int j = 0; j < dictionary.size(); j++) {
+                if (eqal(queries[i], dictionary[j])) {
+                    ans.push_back(queries[i]);
+                    break;  
+                }
+            }
+        }
+
+        return ans;
     }
 };
